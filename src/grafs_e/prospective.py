c9142d1e148c0a4d201c3e0cf353c8ef7056c7ef
Adrien Fauste Gay <adrien.fauste-gay@univ-grenoble-alpes.fr>
2025-06-28 22:03:56 +0200

    Resolve "move urban population to technical tab"

--- src/grafs_e/prospective.py
+++ src/grafs_e/prospective.py
@@ -769,8 +769,8 @@ class scenario:
             ] = self.extrapolate_recent_trend(self.get_import_net(self.region), self.year, seuil_bas=None)[1][-1]
 
             # Proportion urbaine
-            sheets["main"].loc[
-                sheets["main"]["Variable"] == "Urban population",
+            sheets["technical"].loc[
+                sheets["technical"]["Variable"] == "Urban population",
                 "Business as usual",
             ] = self.logistic_urb_pop(self.region, self.year)
 
@@ -2486,7 +2486,7 @@ class NitrogenFlowModel_prospect:
         FE_N_NH3_em = 0.118
         FE_N_N2_em = 0.425
         pop = main.loc[main["Variable"] == "Population", "Business as usual"].item()
-        prop_urb = main.loc[main["Variable"] == "Urban population", "Business as usual"].item() / 100
+        prop_urb = technical.loc[technical["Variable"] == "Urban population", "Business as usual"].item() / 100
         N_cons_cap = main.loc[main["Variable"] == "Total per capita protein ingestion", "Business as usual"].item()
         N_cap_vege = main.loc[main["Variable"] == "Vegetal per capita protein ingestion", "Business as usual"].item()
         N_cap_viande = main.loc[
